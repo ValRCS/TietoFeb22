de9a03a53027be0f47890477905abe1f541384d4
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-02-21 17:48:17 +0200

    Initial commit to show what we did on Febrauary 21st !!

---
+++ feb21.py
@@ -0,0 +1,31 @@
+print("Just a simple script")
+print("Hello Python!")
+print('In Python single quotes work just like double quotes as long as they match')
+print(2*2, 5/2, 10-7, 9000+3)
+print("2 raised to the power of 8 is", 2**8)
+print("20 modulo 9 is ", 20 % 9)
+print(5 % 2, 6 % 2, 7 % 2, 8 % 2, 9 % 2)
+print("Last digit of 941415 is ", 941415 % 10)
+
+# in Python we start using variable as soon as we declare no need for special identifiers
+my_name = "Valdis" # variable
+print("Hello", my_name)
+# since 3.6 we have new way of formatting text
+print(f"Hello {my_name}")  # so f-strings, string interpolation
+print(f"2+2={2+2}")
+print("my_name is of type", type(my_name))
+
+a = 2
+print("a is of type", type(a))
+print("I am still", my_name)
+print(pow(2, 8))  # easier to use 2**8
+print("Valdis"*3)
+print("beer "*7)
+print("Valdis" + " Saulespurens")
+my_full_name = "Valdis" + " Saulespurens"
+print(my_full_name)
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("this will be on new line")
+    print("on 3rd line")
+    print("Rīga dimd, kas to Rīgu dimdināja")
+    print("Emojis work too: 😀 😃 😄 😁 😆")
+    print("2*2 =", 2*2)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Valdis')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
